237939082381c474b7e40a30d864272c4d1a4cd9
weekens <weekens@gmail.com>
2022-11-26 16:09:59 +0400

    Remove unneeded components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,14 @@
 import { ReactElement } from "react";
-import styled from "styled-components";
 import { ActivateDeactivate } from "./components/ActivateDeactivate";
 import { SectionDivider } from "./components/SectionDivider";
-import { SignMessage } from "./components/SignMessage";
-import { WalletStatus } from "./components/WalletStatus";
 import { MutualFund } from "./components/MutualFund";
-
-const StyledAppDiv = styled.div`
-  display: grid;
-  grid-gap: 20px;
-`;
 
 export function App(): ReactElement {
   return (
-    <StyledAppDiv>
+    <div>
       <ActivateDeactivate/>
       <SectionDivider/>
-      <WalletStatus/>
-      <SectionDivider/>
-      <SignMessage/>
-      <SectionDivider/>
       <MutualFund/>
-    </StyledAppDiv>
+    </div>
   );
 }
