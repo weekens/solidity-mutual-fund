cb9a177e9b15040e72f8a94ac10489eb1334ac2a
weekens <weekens@gmail.com>
2022-12-09 12:53:52 +0400

    Display votes in proposals.

--- frontend/src/components/Proposal.tsx
+++ frontend/src/components/Proposal.tsx
@@ -1,6 +1,16 @@
 import { ProposalModel } from "../models/ProposalModel";
 import { ReactElement } from "react";
-import { Grid, Card, CardContent } from "@mui/material";
+import {
+  Grid,
+  Card,
+  CardContent,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  TableContainer,
+  Paper, TableCell, TableHead, TableRow, TableBody, Table
+} from "@mui/material";
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 export function Proposal(props: ProposalModel): ReactElement {
   return (
@@ -25,6 +35,30 @@ export function Proposal(props: ProposalModel): ReactElement {
           <Grid item xs={6}>
             {props.request.proposalType}
           </Grid>
+
+          <Accordion sx={{ width: "100%", marginTop: "15px" }}>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>Votes</AccordionSummary>
+            <AccordionDetails>
+              <TableContainer component={Paper}>
+                <Table aria-label="simple table">
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>Voter</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {props.votes.map(vote => (
+                      <TableRow key={vote.memberAddress}>
+                        <TableCell component="th" scope="row">
+                          {vote.memberAddress}
+                        </TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            </AccordionDetails>
+          </Accordion>
         </Grid>
       </CardContent>
     </Card>
