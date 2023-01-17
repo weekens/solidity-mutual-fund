00b09e1c83400db19c8105c1a1e19ae8e1912788
weekens <weekens@gmail.com>
2023-01-17 10:16:18 +0400

    Fixed deploy script.

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -6,7 +6,8 @@ async function main() {
   const fund = await MutualFund.deploy({
     votingPeriod: 2 * 24 * 60 * 60,
     gracePeriod: 24 * 60 * 60,
-    proposalExpiryPeriod: 5 * 24 * 60 * 60
+    proposalExpiryPeriod: 5 * 24 * 60 * 60,
+    founderName: "Viktor Isaev"
   });
   console.log("Contract deployed to address:", fund.address);
 }
